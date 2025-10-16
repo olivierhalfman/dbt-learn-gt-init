d3d4a81db38acc350e7d8fec12b58bfb4c935a59
Olivier Halfman <olivier.halfman@cz.nl>
2025-10-16 15:00:27 +0000

    created ctes

--- models/staging/jaffle_shop/sgt_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/sgt_jaffle_shop_orders.sql
@@ -1,6 +1,16 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{source('jaffle_shop', 'jaffle_shop_orders')}}
+with
+
+source as (
+    select * from {{source('jaffle_shop', 'jaffle_shop_orders')}}
+),
+
+renamed as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from source
+)
+
+select * from renamed
--- models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -1,5 +1,14 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{source('jaffle_shop', 'jaffle_shop_customers')}}
+with
+source as (
+    select * from {{source('jaffle_shop', 'jaffle_shop_customers')}}
+)
+
+renamed as (
+    select
+        id as customer_id,
+        first_name,
+        last_name
+    from source
+)
+
+select * from renamed
